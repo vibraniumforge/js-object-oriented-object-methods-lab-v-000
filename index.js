47b97afdbd691fabcbe82e6878cb662895a806db
Greg Morgan <gregm9999@gmail.com>
2019-01-12 04:44:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function BoardMember(name, homeState, training) {
+  this.name=name;
+  this.homeState=homeState;
+  this.training=training;
   this.veto=function() {
     console.log("");
   }
-  this.approve
-  this.doCharity
-  this.releasePressStatement
-  this.sayHi
+  this.approve=function() {
+    console.log("");
+  }
+  this.doCharity=function() {
+    console.log("");
+  }
+  this.releasePressStatement=function() {
+    console.log("");
+  }
+  this.sayHi=function() {
+    console.log("");
+  }
 }
