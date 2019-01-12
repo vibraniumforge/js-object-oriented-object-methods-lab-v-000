5899c55e2c477256a99b9e97cde3eaef0a6abbb8
Greg Morgan <gregm9999@gmail.com>
2019-01-12 04:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function BoardMember (name, homeState, training) {
     console.log("You will see great things from Scuber.");
   }
   this.sayHi=function() {
-    console.log(`"Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
+    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
   }
 }
